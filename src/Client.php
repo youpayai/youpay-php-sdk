abaaca6387fc0b2a8c881940c1aa9291cb6d9a72
Jera Simpson <jeramy@mywork.com.au>
2020-06-16 11:18:39 +1000

    Extra endpoints, cleanup and comments

--- src/Client.php
+++ src/Client.php
@@ -8,16 +8,38 @@ namespace YouPaySDK;
  */
 class Client
 {
-    private $api_key;
+    /**
+     * @var string API Token
+     */
+    private $token;
 
-    public static function create($api_key = '')
+    /**
+     * @var string Store ID
+     */
+    private $store_id;
+
+    /**
+     * Client constructor.
+     *
+     * @param string $token
+     * @param string $store_id
+     */
+    public function __construct($token, $store_id = '')
     {
-        return new self($api_key = '');
+        $this->token = $token;
+        $this->store_id = $store_id;
     }
 
-    public function __construct($api_key = '')
+    /**
+     * Create the client
+     *
+     * @param string $token
+     * @param string $store_id
+     * @return Client
+     */
+    public static function create($token, $store_id = '')
     {
-        $this->api_key = $api_key;
+        return new self($token, $store_id);
     }
 
     /**
@@ -27,19 +49,19 @@ class Client
      * @param string $password
      * @param string $domain
      *
-     * @return object [access_token, store_id] || ['status_code', 'message', 'error']
+     * @return object [status_code, access_token, store_id] || [status_code, message, error]
      */
     public static function auth($email, $password, $domain)
     {
         $client = new \GuzzleHttp\Client([
             'base_uri' => 'http://local.youpay.ai/api',
             'timeout'  => 2.0,
-            'headers' => [
+            'headers'  => [
                 'Content-Type' => 'application/json',
             ]
         ]);
 
-        $response = $client->post('/api/login', [
+        $response  = $client->post('/api/login', [
             'json' => [
                 'email'     => $email,
                 'password'  => $password,
@@ -52,60 +74,138 @@ class Client
         );
     }
 
+    /**
+     * List All Store Orders
+     *
+     * @param string
+     * @return array
+     */
     public function listOrders()
     {
         return json_decode(
             $this->client()
-                ->get('/api/order/list')
-                ->getBody()
-                ->getContents()
-        );
-    }
-
-    public function createOrUpdateOrder(Order $order)
-    {
-        return json_decode(
-            $this->client()
-                ->get('/api/order/create', array(
-                    'json' => $order
-                ))
-                ->getBody()
-                ->getContents()
-        );
-    }
-
-    public function  getOrder($id)
-    {
-        return json_decode(
-            $this->client()
-                ->get('/api/order/create', array(
-                    'json' => $order
-                ))
-                ->getBody()
-                ->getContents()
+                ->post( '/api/order/list', [
+                    'json' => [
+                        'store_id' => $this->store_id
+                    ]
+                ])
+                ->getBody()->getContents()
         );
     }
 
     /**
+     * List All Store Orders
+     *
+     * @return array
+     */
+    public function listAllOrders()
+    {
+        return json_decode(
+            $this->client()
+                ->get('/api/order/list' )
+                ->getBody()->getContents()
+        );
+    }
+
+    /**
+     * Create or Update an Order
+     *
+     * @param Order $order
+     * @return mixed
+     */
+    public function createOrUpdateOrder(Order $order)
+    {
+        return json_decode(
+            $this->client()
+                ->post('/api/order', [
+                    'json' => [
+                        'order' => $order,
+                        'store_id' => $this->store_id
+                    ]
+                ])
+                ->getBody()->getContents()
+        );
+    }
+
+    /**
+     * Get the Order by ID
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function getOrder($id)
+    {
+        return json_decode(
+            $this->client()
+                ->get( '/api/order/' . $id )
+                ->getBody()->getContents()
+        );
+    }
+
+    /**
+     * Get the Store by ID
+     *
+     * @param $id
+     * @return mixed
+     */
+    public function getStore($id)
+    {
+        return json_decode(
+            $this->client()
+                ->get( '/api/store/' . $id )
+                ->getBody()->getContents()
+        );
+    }
+
+    /**
+     * Get the Stores
+     *
+     * @return mixed
+     */
+    public function getStores()
+    {
+        return json_decode(
+            $this->client()
+                ->get( '/api/stores/list' )
+                ->getBody()->getContents()
+        );
+    }
+
+    /**
+     * Get the Store by Domain name
+     *
+     * @param string $domain eg. google.com
+     * @return mixed
+     */
+    public function findStore($domain)
+    {
+        return json_decode(
+            $this->client()
+                ->post( '/api/stores/find', [
+                    'json' => [
+                        'domain' => $domain
+                    ]
+                ] )
+                ->getBody()->getContents()
+        );
+    }
+
+    /**
+     * Get the Guzzle Client
+     *
      * @return \GuzzleHttp\Client
      */
     public function client()
     {
-        $options = [
+        return new \GuzzleHttp\Client([
             // Base URI is used with relative requests
             'base_uri' => 'http://local.youpay.ai/api',
             // You can set any number of default request options.
             'timeout'  => 2.0,
             'headers' => [
                 'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer ' . $this->api_key
+                'Authorization' => 'Bearer ' . $this->token
             ]
-        ];
-        return $client = new \GuzzleHttp\Client($options);
-    }
-
-    public function request($path, $data = [], $type = 'GET')
-    {
-
+        ]);
     }
 }
