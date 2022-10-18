7c9e5cb09d09bc2337de9226755067a6cf99929d
Clara Leigh <clara.leigh41@gmail.com>
2022-10-19 09:51:49 +1000

    Revert currency change

--- src/Order.php
+++ src/Order.php
@@ -58,11 +58,6 @@ class Order
     public $total;
 
     /**
-     * @var string Currency code
-     */
-    public $currency_change;
-
-    /**
      * Static way to initialize the class
      *
      * @param array $fillable [order_id, store_id, title, order_items, extra_fees, sub_total, total]
@@ -105,11 +100,6 @@ class Order
             $self->receiver = $fillable['receiver'];
         } else {
             throw new \Exception('Receiver not set.');
-        }
-
-        // Pass through the currency code for which we are changing from to notify the user
-        if ( ! empty($fillable['currency_change'])) {
-            $self->currency_change = $fillable['currency_change'];
         }
 
         return $self;
